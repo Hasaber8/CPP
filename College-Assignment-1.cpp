6cd232a970f01cc5a34e8a0fb41866d61e4cf2f4
Rohan Hasabe <rohanhasabe8@gmail.com>
2020-08-17 15:38:50 +0530

    Cleanup

--- College-Assignment-1.cpp
+++ College-Assignment-1.cpp
@@ -1,13 +1,12 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string.h>
 using namespace std;
 typedef struct student
 {
-int roll_num;
-char name [20];
-float marks;
+    int roll_num;
+    char name [20];
+    float marks;
 }stud;
-
 
 void create(stud s[20],int n);
 void display(stud s[20],int n);
@@ -15,130 +14,124 @@ void bubble_sort(stud s[20],int n);
 void insertionSort(stud s[20],int n);
 void quick_sort(stud s[20],int,int);
 int partition(stud s[20],int,int);
-void search(stud s[20],int n,int key);
+void search(stud s[20],int n,double key);
 int bsearch(stud s[20], char x[20],int low,int high);
 
 int main()
 {
-stud s[20];
-int ch,n,key,result;
-char x[20];
-do
-{
-cout<<"\n 1) Create Student Database ";
-cout<<"\n 2) Display Student Records ";
-cout<<"\n 3) Bubble Sort ";
-cout<<"\n 4) Insertion Sort ";
-cout<<"\n 5) Quick Sort ";
-cout<<"\n 6) Linear search ";
-cout<<"\n 7) Binary search  ";
-cout<<"\n 8) Exit ";
-cout<<"\n Enetr Your Choice:=";
-cin>>ch;
-switch(ch)
-{
-case 1:
-cout<<"\n Enter The Number Of Records:=";
-cin>>n;
-create(s,n);
-break;
-case 2:
-display(s,n);
-break;
-case 3:
-bubble_sort(s,n);
-break;
-case 4:
-insertionSort(s,n);
-break;
-case 5:
-quick_sort(s,0,n-1);
-cout<<"\n"<< "\t"<< "Roll No"<< "\t"<<" Name" <<"\t"<< "Marks";
-for(int i=n-1; i>=n-10; i--)
-{
-	cout<<"\n";
-cout<<"\t "<< s[i].roll_num<<"\t "<<s[i].name<<"\t "<<s[i].marks;
+    stud s[20];
+    int ch,n,key,result;
+    char x[20];
+    do
+    {
+        cout<<"\n 1) Create Student Database ";
+        cout<<"\n 2) Display Student Records ";
+        cout<<"\n 3) Bubble Sort (To sort Roll Number wise) ";
+        cout<<"\n 4) Insertion Sort (To sort according to the Name) ";
+        cout<<"\n 5) Quick Sort (To sort according to the marks)";
+        cout<<"\n 6) Linear search ";
+        cout<<"\n 7) Binary search  ";
+        cout<<"\n 8) Exit ";
+        cout<<"\n Enter Your Choice:=";
+        cin>>ch;
+        switch(ch)
+        {
+            case 1:
+            cout<<"\n Enter The Number Of Records:=";
+            cin>>n;
+            create(s,n);
+            break;
 
-}
-break;
-case 6:
-cout<<"\n Enter the marks which u want to search:=";
-cin>>key;
-search(s,n,key);
-break;
+            case 2:
+            display(s,n);
+            break;
 
+            case 3:
+            bubble_sort(s,n);
+            break;
 
+            case 4:
+            insertionSort(s,n);
+            break;
 
-case 7:
-cout<<"\n Enter the name of student  which u want to search:=";
-cin>>x;
+            case 5:
+            quick_sort(s,0,n-1);
+            cout<<"\n"<< "\t"<< "Roll No"<< "\t"<<" Name" <<"\t"<< "Marks";
+            for(int i=n-1; i>=n-10; i--)
+            {
+                cout<<"\n";
+            cout<<"\t "<< s[i].roll_num<<"\t "<<s[i].name<<"\t "<<s[i].marks;
+            }
+            break;
 
+            case 6:
+            cout<<"\n Enter the marks which u want to search:=";
+            cin>>key;
+            search(s,n,key);
+            break;
 
+            case 7:
+            cout<<"\n Enter the name of student  which u want to search:=";
+            cin>>x;
+                insertionSort(s,n);
+                result=bsearch(s,x,0,(n-1));
+                if(result==-1)
+                {
+                cout<<" \n Student name you want to search for is not present ! \n";
+                }
+                else
+                {
+                cout<<" \n The student  is present :\t" << s[result].name;
+                }
+                break;
 
-       insertionSort(s,n);
-       result=bsearch(s,x,0,(n-1));
-       if(result==-1)
-       {
-       cout<<" \n Student name you want to search for is not present ! \n";
-       }
-       else
-       {
-       cout<<" \n The student  is present :\t" << s[result].name;
-       }
-       break;
-
-
-
-case 8:return 0;
-      default:cout<<"\n Invalid choice !! Please enter your choice again."<<endl;
-}
-}while(ch!=8);
+            case 8:return 0;
+                default:cout<<"\n Invalid choice !! Please enter your choice again."<<endl;
+        }
+    }while(ch!=8);
 }
 void create(stud s[20],int n)
 {
-int i;
-for(i=0;i<n;i++)
-{
-cout<<"\n Enter the roll number:=";
-cin>>s[i].roll_num;
-cout<<"\n Enter the Name:=";
-cin>>s[i].name;
-cout<<"\n Enter the marks:=";
-cin>>s[i].marks;
-}
+    int i;
+    for(i=0;i<n;i++)
+    {
+        cout<<"\n Enter the roll number:=";
+        cin>>s[i].roll_num;
+        cout<<"\n Enter the Name:=";
+        cin>>s[i].name;
+        cout<<"\n Enter the marks:=";
+        cin>>s[i].marks;
+    }
 }
 void display(stud s[20],int n)
 {
-int i;
-cout<<"\n"<< "\t"<< "Roll No"<< "\t"<<" Name" <<"\t"<< "Marks";
-for(i=0;i<n;i++)
-{
-	cout<<"\n";
-cout<<"\t "<< s[i].roll_num<<"\t "<<s[i].name<<"\t "<<s[i].marks;
+    int i;
+    cout<<"\n"<< "\t"<< "Roll No"<< "\t"<<" Name" <<"\t"<< "Marks";
+    for(i=0;i<n;i++)
+    {
+        cout<<"\n";
+    cout<<"\t "<< s[i].roll_num<<"\t "<<s[i].name<<"\t "<<s[i].marks;
+    }
 }
-}
-
-
 
 //bubble sort to sort in ascending order on roll number
 
-
 void bubble_sort(stud s[20],int n)
 {
-int i,j;
-stud temp;
-for(i=1;i<n;i++)
-{
-for(j=0;j<n-i;j++)
-{
-if(s[j].roll_num>s[j+1].roll_num)
-{
-temp=s[j];
-s[j]=s[j+1];
-s[j+1]=temp;
-}
-}
-}
+    int i,j;
+    stud temp;
+    for(i=1;i<n;i++)
+    {
+        for(j=0;j<n-i;j++)
+        {
+            if(s[j].roll_num>s[j+1].roll_num)
+            {
+                temp=s[j];
+                s[j]=s[j+1];
+                s[j+1]=temp;
+            }
+        }
+    }
 }
 
 // insertion sort to sort on names in ascending order
@@ -164,8 +157,6 @@ void insertionSort(stud s[20], int n)
     }  
 }  
   
-
-
 //Quick sort to sort on marks 
 
 void quick_sort(stud s[20], int l,int u)
@@ -213,46 +204,39 @@ int partition(stud s[20], int l,int u)
     return(j);
 }
 
-
 // linear search for marks if more than one student having same marks print all of them 
 
-void search(stud s[20],int n,int key)
+void search(stud s[20],int n,double key)
 {
-int i;
-
-cout<<"\n"<< "\t"<< "Roll No"<< "\t"<<" Name" <<"\t"<< "Marks";
-
-
-for(i=0;i<n;i++)
-{
-if(key==s[i].marks)
-{
-cout<<"\n\t "<< s[i].roll_num<<"\t "<<s[i].name<<"\t "<<s[i].marks;
+    int i;
+    cout<<"\n"<< "\t"<< "Roll No"<< "\t"<<" Name" <<"\t"<< "Marks";
+    for(i=0;i<n;i++)
+    {
+        if(key==s[i].marks)
+        {
+            cout<<"\n\t "<< s[i].roll_num<<"\t "<<s[i].name<<"\t "<<s[i].marks;
+        }
+    }
 }
-
-}
-
-}
-
 
 int bsearch(stud s[20], char x[20],int low,int high)
 {
-int mid;
-while(low<=high)
-{
-mid=(low+high)/2;
-if(strcmp(x,s[mid].name)==0)
-{
-return mid;
-}
-else if(strcmp(x,s[mid].name)<0)
-{
-high=mid-1;
-}
-else
-{
-low=mid+1;
-}
-}
-return -1;
+    int mid;
+    while(low<=high)
+    {
+        mid=(low+high)/2;
+        if(strcmp(x,s[mid].name)==0)
+        {
+            return mid;
+        }
+        else if(strcmp(x,s[mid].name)<0)
+        {
+            high=mid-1;
+        }
+        else
+        {
+            low=mid+1;
+        }
+    }
+    return -1;
 }
