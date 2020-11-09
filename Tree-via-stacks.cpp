a5d476b816496cc8a582d78039f639981e92bf08
Rohan Hasabe <Rohanhasabe8@gmail.com>
2020-11-09 15:46:47 +0530

    bunch of assignments

--- Tree-via-stacks.cpp
+++ Tree-via-stacks.cpp
@@ -1,273 +1,309 @@
-#include <iostream>
+#include <cstdlib>
+#include "iostream"
+
 using namespace std;
-typedef struct node
-{ 
+
+typedef struct node 
+{
     char data;
     struct node *left;
     struct node *right;
-}node;
+} node;
 
-typedef struct stacknode
+typedef struct stacknode 
 {
-    node *data;
+    node* data;
     struct stacknode *next;
-}stacknode;
+} stacknode;
 
-class stack
+class stack 
 {
     stacknode *top;
     public:
-    stack()
+
+    stack() 
     {
-        top=NULL;
+        top = NULL;
     }
-    
-    node *topp()
+
+    node* topp() 
     {
-        return (top -> data);
+        return top->data;
     }
-    
-    int isempty()
+
+    int isempty() 
     {
-        if(top==NULL)
+        if (top == NULL) 
+        {
             return 1;
+        }
         return 0;
     }
-    
-    void push(node* a)
+
+    void push(node* a) 
     {
         stacknode *p;
-        p=new stacknode();
-        p->data=a;
-        p->next=top;
-        top=p;
+        p = new stacknode();
+        p->data = a;
+        p->next = top;
+        top = p;
     }
-    
-    node* pop()
+
+    node* pop() 
     {
         stacknode *p;
         node* x;
-        x=top->data;
-        p=top;
-        top=top->next;
+        x = top->data;
+        p = top;
+        top = top->next;
 
         return x;
     }
 };
 
-node* create_per(char prefix[10]);
-node* create_post(char postfix[10]);
+node* create_pre(char prefix[10]);
+node* create_post(char postfix[20]);
 void inorder_non_recursive(node *t);
 void inorder(node *p);
 void preorder(node *p);
 void postorder(node *p);
+void preorder_non_recursive(node* t);
 void postorder_non_recursive(node *t);
-void preorder_non_recursive(node *t);
 
-node* create_post(char postfix[10])
-{
-    node *p,*p1,*p2;
-    stack s;
-        for (int i=0;postfix[i]!='\0';i++)
-        {
-            char token=postfix[i];
-            if(isalnum(token))
-            {
-                p=new node();
-                p->data=token;
-                p->left=NULL;
-                p->right=NULL;
-                s.push(p);
-            }
-            else
-            {
-                p1 = s.pop();
-                p2 = s.pop();
-                p=new node();
-                p->data=token;
-                p->left=p2;
-                p->right=p1;
-                s.push(p);
-            }
-        }
-        return s.pop();
-}
-
-node* create_per(char prefix[10])
+node* create_post(char postfix[10]) 
 {
     node *p;
     stack s;
-    int i;
-        for (int i;prefix[i]!='\0';i++)
-        {
-
-        }
-        i=i-1;
-    for(;i>=0;i--)
+    for (int i = 0; postfix[i] != '\0'; i++) 
     {
-        char token=prefix[i];
-        if(isalnum(token))
+        char token = postfix[i];
+        if (isalnum(token)) 
         {
-            p=new node();
-            p->data=token;
-            p->left=NULL;
-            p->right=NULL;
+            p = new node();
+            p->data = token;
+            p->left = NULL;
+            p->right = NULL;
             s.push(p);
-        }
-        else
+        } 
+        else 
         {
-            p=new node();
-            p->data=token;
-            p->left=s.pop();
-            p->right=s.pop();
+            p = new node();
+            p->data = token;
+            p->right = s.pop();
+            p->left = s.pop();
             s.push(p);
         }
     }
     return s.pop();
 }
 
-void inorder(node *p)
+node* create_pre(char prefix[10]) 
 {
-    if(p!=NULL)
+    node *p;
+    stack s;
+    int i;
+    for (i = 0; prefix[i] != '\0'; i++) {
+    }
+    i = i - 1;
+    for (; i > 0; i--) 
+    {
+        char token = prefix[i];
+        if (isalnum(token)) 
+        {
+            p = new node();
+            p->data = token;
+            p->left = NULL;
+            p->right = NULL;
+            s.push(p);
+        } 
+        else 
+        {
+            p = new node();
+            p->data = token;
+            p->left = s.pop();
+            p->right = s.pop();
+            s.push(p);
+        }
+    }
+    return s.pop();
+}
+
+
+void inorder(node *p) 
+{
+    if (p != NULL) 
     {
         inorder(p->left);
-        cout<<p -> data;
+        cout << p->data;
         inorder(p->right);
     }
 }
 
-void preorder(node *p)
+void preorder(node *p) 
 {
-    if(p!=NULL)
+    if (p != NULL) 
     {
-        cout<<p -> data;
+        cout << p->data;
         preorder(p->left);
         preorder(p->right);
     }
 }
 
-void postorder(node *p)
+void postorder(node *p) 
 {
-    if(p!=NULL)
+    if (p != NULL) 
     {
         postorder(p->left);
         postorder(p->right);
-        cout<<p->data;
+        cout << p->data;
     }
 }
 
-void inorder_non_recursive(node *t)
+void inorder_non_recursive(node *t) 
 {
     stack s;
-    while(t!=NULL)
+    while (t != NULL) 
     {
         s.push(t);
-        t=t->left;
+        t = t->left;
     }
-    while(s.isempty()!=1)
+    while (s.isempty() != 1) 
     {
-        t=s.pop();
-        cout<<t->data;
-        t=t->right;
-        while(t!=NULL)
+        t = s.pop();
+        cout << t->data;
+        t = t->right;
+        while (t != NULL) 
         {
             s.push(t);
-            t=t->left;
+            t = t->left;
         }
     }
 }
 
-void preorder_non_recursive(node *t)
+void preorder_non_recursive(node *t) 
 {
     stack s;
-    while(t!=NULL)
+    while (t != NULL) 
     {
-        cout<<t->data;
+        cout << t->data;
         s.push(t);
-        t=t->left;
+        t = t->left;
     }
-    while(s.isempty()!=1)
+    while (s.isempty() != 1) 
     {
-        t=s.pop();
-        t=t->right;
-        while(t!=NULL)
+        t = s.pop();
+        t = t->right;
+        while (t != NULL) 
         {
-            cout<<t->data;
+            cout << t->data;
             s.push(t);
-            t=t->left;
+            t = t->left;
         }
     }
 }
 
-void postorder_non_recursive(node *t)
+void postorder_non_recursive(node *t) 
 {
-    stack s;
-    while(t!=NULL)
+    stack s, s1;
+    node *t1;
+    while (t != NULL) 
     {
-        cout<<t->data;
         s.push(t);
-        t=t->left;
+        s1.push(NULL);
+        t = t->left;
     }
-    while(s.isempty()!=1)
+    while (s.isempty() != 1) 
     {
-        t=s.pop();
-        t=t->right;
-        while(t!=NULL)
+        t = s.pop();
+        t1 = s1.pop();
+        if (t1 == NULL) 
         {
-            cout<<t->data;
             s.push(t);
-            t=t->left;
+            s1.push((node *) 1);
+            t = t->right;
+            while (t != NULL) 
+            {
+                s.push(t);
+                s1.push(NULL);
+                t = t->left;
+            }
+        } 
+        else 
+        {
+            cout << t->data;
         }
     }
 }
 
-int main()
+int main() 
 {
-    node *r=NULL, *r1;
-    char postfix[10],prefix[10];
+    node *r = NULL, *r1;
+    char postfix[10], prefix[10];
     int x;
-    int ch,choice,result;
-    do
+    int ch, choice;
+    do 
     {
-        cout<<"\n\t****TREE OPERATIONS****\n1. Construct tree from postfix / prefix expression";
-        cout<<"\n2. Inorder Traversal of tree";
-        cout<<"\n3. Preorder Traversal of tree";
-        cout<<"\n4. Postorder Traversal of tree";
-        cin>>ch;
-        switch(ch)
+        cout << " \t ****TREE OPERATIONS****" << endl;
+        cout << "1) Construct tree from postfix expression/prefix expression " << endl;
+        cout << "2) Inorder Traversal of Tree" << endl;
+        cout << "3) Preorder traversal" << endl;
+        cout << "4) Postorder traversal" << endl;
+        cout << "5) EXIT" << endl;
+        cout << "Enter your choice : ";
+        cin >> ch;
+        cout << endl;
+        switch (ch) 
         {
-            case 1:cout<<"ENTER CHOICE: \n1. PostFix Expression \n2. Prefix Expression";
-                cin>>choice;
-                if(choice==1)
+            case 1:
+                cout << "1) Postfix expression" << endl;
+                cout << "2) Prefix expression" << endl;
+                cout << "ENTER YOUR CHOICE :";
+                cin >> choice;
+                cout << endl;
+                if (choice == 1) 
                 {
-                    cout<<"\nEnter postfix Expression:- ";
-                    cin>>postfix;
-                    r=create_post(postfix);
-                }
-                else
+                    cout << "Enter postfix expression : " << endl;
+                    cin >> postfix;
+                    r = create_post(postfix);
+                } 
+                else 
                 {
-                    cout<<"\nEnter prefix Expression:- ";
-                    cin>>prefix;
-                    r=create_per(prefix);
+                    cout << "Enter prefix expression : " << endl;
+                    cin >> prefix;
+                    r = create_pre(prefix);
                 }
-                cout<<"\n\nTree created succesfully";
+                cout << endl;
+                cout << "Tree created successfully" << endl;
+                cout << endl;
                 break;
-            case 2:cout<<"Inorder Traversal of tree:\n";
+            case 2:
+                cout << "Inorder Traversal of Tree with recursion : ";
                 inorder(r);
-                cout<<"\nInorder traversal without recursion:- ";
+                cout << endl;
+                cout << "Inorder Traversal of Tree without recursion : ";
                 inorder_non_recursive(r);
+                cout << endl;
+                cout << endl;
                 break;
-            case 3:cout<<"Postorder Traversal of tree:\n";
-                postorder(r);
-                cout<<"\nPostorder traversal without recursion:- ";
-                postorder_non_recursive(r);
-                break;
-            case 4:cout<<"Preorder traversal of tree:\n";
+            case 3:
+                cout << "Preorder traversal with recursion of tree ";
                 preorder(r);
-                cout<<"\nPreorder traversal without recursion:- ";
+                cout << endl;
+                cout << "Preorder traversal without recursion : ";
                 preorder_non_recursive(r);
+                cout << endl;
+                cout << endl;
+                break;
+            case 4:
+                cout << "Postorder traversal with recursion of tree : ";
+                postorder(r);
+                cout << endl;
+                cout << "Postorder traversal without recursion of tree : ";
+                postorder_non_recursive(r);
+                cout << endl;
+                cout << endl;
                 break;
         }
-    }while(ch!=5);
-        return 0;
+    } while (ch != 5);
+    return 0;
 }
--- inorder-traversal-tree.cpp
+++ inorder-traversal-tree.cpp
@@ -3,15 +3,16 @@ using namespace std;
 
 struct node 
 {
-   char d;
+   char data;
    node *left;
    node *right;
 };
 
-char pf[50];
+char PostFix[50];
 int top = -1;
-n *a[50];
-int r(char inputch) {
+node *a[50];
+int r(char inputch) 
+{
    if (inputch == '+' || inputch == '-' || inputch == '*' || inputch== '/')
       return (-1);
    else if (inputch >= 'A' || inputch <= 'Z')
@@ -22,70 +23,118 @@ int r(char inputch) {
       return (-100);
 }
 
-void push(n *tree) {
+int isempty()
+{
+    if(top==NULL)
+        return 1;
+    return 0;
+}
+
+void push(node *tree) 
+{
    top++;
    a[top] = tree;
 }
 
-n *pop() {
+node *pop() 
+{
    top--;
    return (a[top + 1]);
 }
 
-void construct_expression_tree(char *suffix) {
+void create_post(char *suffix) 
+{
    char s;
-   n *newl, *p1, *p2;
+   node *newl, *p1, *p2;
    int flag;
    s = suffix[0];
-   for (int i = 1; s != 0; i++) {
+   for (int i = 1; s != 0; i++) 
+   {
       flag = r(s);
-      if (flag == 1) {
-         newl = new n;
-         newl->d = s;
-         newl->l = NULL;
-         newl->r = NULL;
+      if (flag == 1) 
+      {
+         newl = new node;
+         newl->data = s;
+         newl->left = NULL;
+         newl->right = NULL;
          push(newl);
-      } else {
+      } 
+      else 
+      {
          p1 = pop();
          p2 = pop();
-         newl = new n;
-         newl->d = s;
-         newl->l = p2;
-         newl->r = p1;
+         newl = new node;
+         newl->data = s;
+         newl->left = p2;
+         newl->right = p1;
          push(newl);
       }
       s = suffix[i];
    }
 }
-void preOrder(n *tree) {
-   if (tree != NULL) {
-      cout << tree->d;
-      preOrder(tree->l);
-      preOrder(tree->r);
+
+void preOrder(node *tree) 
+{
+   if (tree != NULL) 
+   {
+      cout << tree->data;
+      preOrder(tree->left);
+      preOrder(tree->right);
    }
 }
-void inOrder(n *tree) {
-   if (tree != NULL) {
-      inOrder(tree->l);
-      cout << tree->d;
-      inOrder(tree->r);
+
+void inOrder(node *tree) 
+{
+   if (tree != NULL) 
+   {
+      inOrder(tree->left);
+      cout << tree->data;
+      inOrder(tree->right);
    }
 }
-void postOrder(n *tree) {
-   if (tree != NULL) {
-      postOrder(tree->l);
-      postOrder(tree->r);
-      cout << tree->d;
+
+void postOrder(node *tree) 
+{
+   if (tree != NULL) 
+   {
+      postOrder(tree->left);
+      postOrder(tree->right);
+      cout << tree->data;
    }
 }
-int main(int argc, char **argv) {
+
+void postorder_non_recursive(node *tree)
+{
+    while(tree !=NULL)
+    {
+        cout<<tree->data;
+        push(tree);
+        tree=tree->left;
+    }
+    while(isempty()!=1)
+    {
+        tree=pop();
+        tree=tree->right;
+        while(tree !=NULL)
+        {
+            cout<<tree->data;
+            push(tree);
+            tree=tree->left;
+        }
+    }
+}
+
+int main(int argc, char **argv) 
+{
    cout << "Enter Postfix Expression : ";
-   cin >> pf;
-   construct_expression_tree(pf);
+   cin >> PostFix;
+   create_post(PostFix);
    cout << "\nIn-Order Traversal : ";
    inOrder(a[0]);
    cout << "\nPre-Order Traversal : ";
    preOrder(a[0]);
+   cout << "\nPost-Order NonRecursive Traversal : ";
+   postorder_non_recursive(a[0]);
    cout << "\nPost-Order Traversal : ";
    postOrder(a[0]);
    return 0;
